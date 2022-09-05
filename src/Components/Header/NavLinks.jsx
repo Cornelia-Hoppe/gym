950499f45a091c0f688fa7d99c7e93d8d934a798
Asal Shamim <41086746+asalshamim@users.noreply.github.com>
2022-09-05 23:02:18 +0200

    o

--- src/Components/Header/NavLinks.jsx
+++ src/Components/Header/NavLinks.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 
+
+
 const Navlink = () => {
   return (
     <ul>
       <li><h2>Hejsan</h2></li>
-      <li><h2>Hejsan</h2></li>
+
       <li><h2>Hejsan</h2></li>
       <li><h2>Hejsan</h2></li>
     </ul>
--- src/Components/Home/Homepage.jsx
+++ src/Components/Home/Homepage.jsx
@@ -6,17 +6,19 @@ import Footer from '../Footer/Footer';
 
 
 
+
 const Home = () => {
   return (
     <>
-    <main>
+      <main>
+        
    
       <div className={style.HomePage}>
                 <div className={style.LandingPage}>
                     <div className={style.gymImgOne}><img src={gym} alt="gym" /></div>
                     <div className={style.LandingText}><h1>HEJ</h1></div>
                 </div>
-                
+               
       </div>
       </main>
       <Footer/>
