3ba28c67ea90955b5f0d112b10e892264fcdab02
Benjamin Sahlgren <bensahlgren@gmail.com>
2022-09-13 14:30:31 +0200

    footer design

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,64 +1,57 @@
-import React from 'react';
+import React from "react";
 import "./Footer.css";
-import {FaFacebook, FaTwitter, FaInstagram, FaLinkedin} from 'react-icons/fa';
+import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from "react-icons/fa";
 
-function Footer()  {
-   
-    return (
-        <footer className='footer'>
-            <div className="footer-container" >
-           
-                <ul>
-                   
-            <div className="wrapper">
-                            <li className="footer-sub-start" path='/'>Startsida</li>
-                            <li className="footer-sub-persion" >Personal</li>
-                
-                <li className="footer-sub-bok">Boka pass</li>
-                    <li className="footer-sub-web">Webbshop</li>
-                        <li className="footer-sub-mina">Mina Sidor</li>
-                       
-                        </div>
-                    </ul>
-                    
-                        
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        <div className="wrapper">
+          <div className="footer-sub-start" id="footer-link">
+            <strong className="footer-subheading">Startsida</strong>
+          </div>
+          <div className="footer-sub-persion" id="footer-link">
+            <strong className="footer-subheading">Personal</strong>
+          </div>
 
-                
+          <div className="footer-sub-bok" id="footer-link">
+            <strong className="footer-subheading">Boka pass</strong>
+          </div>
+          <div className="footer-sub-web" id="footer-link">
+            <strong className="footer-subheading">Webbshop</strong>
+          </div>
+          <div className="footer-sub-mina" id="footer-link">
+            <strong className="footer-subheading">Mina Sidor</strong>
+          </div>
+        </div>
 
-            {/* ------------------------------ footer social-media ------------------------- */}
-            
+        <section id="social-section">
+          <div className="social">
             <strong className="footer-subheading">Följ oss!</strong>
-                <div className="footer-icons">
-                <FaInstagram className="footer-icon-in"/>
-                    <FaFacebook className="footer-icon-fa"/>
-                    
-                    <FaTwitter className="footer-icon-tw"/>
-                    <FaLinkedin className="footer-icon-li"/>
-                </div>
-
-                {/* ------------------------------ footer Kontakt ------------------------- */}
-                <div className="footer-sub location">
-                <strong className="footer-contact">Kontakta oss</strong> 
-                   
-                
-                </div>
-                
-                <span className="footer-desc">
-                    Telefon: 070-111 22 33
-               <div> E-post: info@sportix.se</div>
-
-                Pusterviksgatan 3
-            413 01
-            Göteborg
-                        <br/>
-                     
-                    </span>
-                   
+            <div className="footer-icons">
+              <FaFacebook className="footer-icon" />
+              <FaInstagram className="footer-icon" />
+              <FaTwitter className="footer-icon" />
+              <FaLinkedin className="footer-icon" />
             </div>
-            
-        </footer>
-        
-    )
-}
+          </div>
+
+          <div className="contact">
+            <div className="alt-location">
+              <strong className="footer-subheading">Kontakta oss</strong>
+            </div>
+            <span className="footer-desc">
+              Telefon: 070-111 22 33
+              <div> E-post: info@sportix.se</div>
+              Pusterviksgatan 3 <br></br>
+              413 01 Göteborg
+              <br />
+            </span>
+          </div>
+        </section>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
