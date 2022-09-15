c09b06f975ac183c8a5c00570f333b1e51c876bd
Yrla Hackzell <yrla.hackzell@hotmail.com>
2022-09-15 11:57:55 +0200

    gallery arrows

--- src/Components/HomeGallery/HomeGallery.jsx
+++ src/Components/HomeGallery/HomeGallery.jsx
@@ -1,51 +1,63 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import style from "./HomeGallery.module.css";
-import img1 from "../../images/gallery1.jpg";
-import img2 from"../../images/gallery2.jpg";
-import img3 from"../../images/gallery3.jpg";
-import img4 from"../../images/gallery4.jpg";
-
-
-
-
-
+import { imgs } from "./GalleryImgs";
 
 const HomeGallery = () => {
-    const imgs=[
-        {id:0,value:img1},
-        {id:1,value:img2},
-        {id:2,value:img3},
-        {id:3,value:img4},
-    ]
 
-    const [sliderData,setSliderData]=useState(imgs[0])
-    const handleClick=(index)=>{
+  const [sliderData, setSliderData] = useState(imgs[0]);
+  const handleClick = (index) => {
     console.log(index);
-    const slider=imgs[index];
+    const slider = imgs[index];
     setSliderData(slider);
-}
-  return (
-    
-    <>
-        <div className={style.GallerySection}>
-            <div className={style.GalleryInfo}> 
-              <h3>Galleri</h3>
-              <p>Kolla in bilder på våra lokaler och vår utrustning!</p>
-             </div>
-            <div className={style.prevBtn}><p>Förra</p></div>
-            <div className={style.nextBtn}><p>Nästa</p></div>
-            <img src={sliderData.value} height="180" width="300" />
-
-            <div className={style.flexrow}>
-            {
-            imgs.map((data,i)=>
-            <img key={data.id} src={data.value} onClick={()=>handleClick(i)} height="70" width="100" />
-            )
-            }
-            </div>
-        </div>
-    </>    
-    );
   };
-  
-  export default HomeGallery;
+  const PrevBtn = () => {
+    if (sliderData.id == 0) {
+      const lastImg = imgs[3];
+      setSliderData(lastImg);
+    } else {
+      const nextImg = imgs[sliderData.id - 1];
+      setSliderData(nextImg);
+    }
+  };
+  const NextBtn = () => {
+    if (sliderData.id == 3) {
+      const firstImg = imgs[0];
+      setSliderData(firstImg);
+    } else {
+      const prevImg = imgs[sliderData.id + 1];
+      setSliderData(prevImg);
+    }
+  };
+  return (
+    <>
+      <div className={style.GallerySection}>
+        <div className={style.GalleryInfo}>
+          <h3>Galleri</h3>
+          <p>Kolla in bilder på våra lokaler och vår utrustning!</p>
+        </div>
+        <div onClick={PrevBtn} className={style.prevBtn}>
+          <p>Förra</p>
+        </div>
+
+        <div onClick={NextBtn} className={style.nextBtn}>
+          <p>Nästa</p>
+        </div>
+        <img src={sliderData.value} height="180" width="300" />
+
+        <div className={style.flexrow}>
+          {imgs.map((data, i) => (
+            <img
+              key={data.id}
+              src={data.value}
+              onClick={() => handleClick(i)}
+              height="70"
+              width="100"
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HomeGallery;
