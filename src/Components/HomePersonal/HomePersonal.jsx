0af5b4321d8c71ef66a6ac9111638f0cf55b9b9b
Cornelia-Hoppe <90331417+Cornelia-Hoppe@users.noreply.github.com>
2022-09-20 13:15:58 +0200

    home

--- src/Components/HomePersonal/HomePersonal.jsx
+++ src/Components/HomePersonal/HomePersonal.jsx
@@ -1,9 +1,5 @@
 import React from 'react'
 import style from "./HomePersonal.module.css";
-import personal1 from "../../images/personal1.jpg"
-import personal2 from "../../images/personal2.jpg"
-import personal3 from "../../images/personal3.jpg"
-import personal4 from "../../images/personal4.jpg"
 
 
 const HomePersonal = () => {
@@ -14,10 +10,10 @@ const HomePersonal = () => {
               <h3>Personal</h3>
         </div>
         <div className={style.PersonalButtons}>
-            <button className={style.persona1} style={{ backgroundImage: `url(${personal1})` }}><div className={style.pesonaltext}><p>Ledning</p></div></button>
-            <button className={style.persona2} style={{ backgroundImage: `url(${personal2})` }}><p>Tränare</p></button>
-            <button className={style.persona3} style={{ backgroundImage: `url(${personal3})` }}><p>Reception</p></button>
-            <button className={style.persona4} style={{ backgroundImage: `url(${personal4})` }}><p>Instruktörer</p></button>
+            <button className={style.personal1}><p>Ledning</p></button>
+            <button className={style.personal2}><p>Tränare</p></button>
+            <button className={style.personal3}><p>Reception</p></button>
+            <button className={style.personal4}><p>Instruktörer</p></button>
         </div>
     </div>
     </>
